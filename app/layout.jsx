05192de42cb0fc339f0866608f0037d66ea4d5dc
nathanwag15 <65185723+nathanwag15@users.noreply.github.com>
2023-01-29 00:54:08 -0700

    day one update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
-import './globals.css'
+import "./globals.css";
+import { Montserrat } from "@next/font/google";
 
+const montserrat = Montserrat({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -8,7 +14,7 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`${montserrat.className}`}>{children}</body>
     </html>
-  )
+  );
 }
---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Movie from "./movie";
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
+  const res = await data.json();
+  console.log(res);
+  return (
+    <main>
+      <h1 className="">Hello next 13 🔥 </h1>
+      {res.results.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+        />
+      ))}
+    </main>
+  );
+}
